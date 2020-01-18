c5994706763ee191bf1f5328c74adf66da38313d
Th5tch3r <amber.phan27@gmail.com>
2020-01-18 12:44:10 -0500

    all get request done, working on post

--- data/db-router.js
+++ data/db-router.js
@@ -14,6 +14,36 @@ router.get('/', (req, res) => {
         })
 })
 
+//GET /api/posts/:id
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
 
+    db.findById(id)
+        .then(post => {
+            if (post.length != 0) {
+                res.status(200).json(post)
+            } else  {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The post information could not be retrieved.", err })
+        })
+})
+
+//GET /api/posts/:id/comments
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const comments = await db.findPostComments(req.params.id);
+        if (comments.length > 0) {
+            res.status(200).json(comments)
+        } else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        } 
+    } catch(error) {
+        console.log(error)
+        res.status(500).json({ error: "The comments information could not be retrieved.", error })
+    }
+})
 
 module.exports = router;
