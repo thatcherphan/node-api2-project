2583ff3f3acbb38265ef0872c96faf25be4f2d65
Th5tch3r <amber.phan27@gmail.com>
2020-01-23 13:24:08 -0500

    deploying with dotenv and heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const server = require('./server');
 
-const port = 4000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
     console.log(`Server is alive on port ${port}`)
